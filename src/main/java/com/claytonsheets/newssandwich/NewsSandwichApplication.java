23fcc5a19a5fb8412e51c881958137ea7ead6eb5
claytonsheets7 <claytonsheets7@gmail.com>
2018-08-01 14:35:11 -0700

    Initial commit

---
+++ src/main/java/com/claytonsheets/newssandwich/NewsSandwichApplication.java
@@ -0,0 +1,12 @@
+package com.claytonsheets.newssandwich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSandwichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsSandwichApplication.class, args);
+	}
+}
